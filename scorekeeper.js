a55b1dcf17a6d999110c2009f6aefb3d1f445eea
U-CORP\rsingh <rsingh@WIN-GFL65PIU0E8.corp.amazonworkspaces.com>
2017-03-21 13:59:25 -0400

    final commit

--- scorekeeper.js
+++ scorekeeper.js
@@ -6,13 +6,16 @@ p1score.innerHTML = p1initialscore;
 
 p1button.addEventListener("click", function() {
 	if (p1score.innerHTML !== maxscore.value) {
-		p1initialscore++;
-		p1score.innerHTML = p1initialscore;
 		if (p2score.innerHTML !== maxscore.value) {
 			p1initialscore++;
 			p1score.innerHTML = p1initialscore;
 		}
 	}
+	if (p1score.innerHTML === maxscore.value) {
+		p1score.style.color = "green";
+	}
+
+
 });
 
 // player two button
@@ -22,8 +25,13 @@ var p2initialscore = 0;
 p2score.innerHTML = p2initialscore;
 p2button.addEventListener("click", function() {
 	if (p2score.innerHTML !== maxscore.value) {
-		p2initialscore++;
-		p2score.innerHTML = p2initialscore;
+		if (p1score.innerHTML !== maxscore.value) {
+			p2initialscore++;
+			p2score.innerHTML = p2initialscore;
+		}
+	}
+	if (p2score.innerHTML === maxscore.value) {
+		p2score.style.color = "green";
 	}
 });
 
@@ -34,11 +42,15 @@ reset.addEventListener("click", function() {
 	p2initialscore = 0;
 	p1score.innerHTML = p1initialscore;
 	p2score.innerHTML = p2initialscore;
+	p1score.style.color = "black";
+	p2score.style.color = "black";
 });
 
 // changing maxscore
 var displaymaxscore = document.getElementById("displaymaxscore");
 var maxscore = document.getElementById("maxscore");
+displaymaxscore.innerHTML = 5;
+maxscore.value = 5;
 maxscore.addEventListener("change", function() {
 	displaymaxscore.innerHTML = maxscore.value;
 });
